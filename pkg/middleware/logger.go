e78aa2226ba75910b862a06da1da6fdeb229d0b1
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-12 21:43:12 +0600

    :sparkles: renamed existing file and created a new logger middleware

---
+++ pkg/middleware/logger.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+// LogRequest - uses as a middleware to log request in console
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
+}
